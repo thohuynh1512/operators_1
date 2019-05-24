adab290ed56164e8ffd2f459a9deadaf30a39c72
thohuynh1512 <thohuynh1512@gmail.com>
2019-05-24 20:19:15 +0700

    completed

---
+++ operators_1.cpp
@@ -0,0 +1,19 @@
+/*
+Evaluate these expressions and place your answer on the answer sheet:
+a.   2 + 3 * 4 - 5
+b.   14 + 8 * 2
+c.   17 * (( 3 - 1 ) * 7 )
+d.   2.0 + 3.0 * (2.0 - 6.0 * 4.0)
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " 2 + 3 * 4 - 5 = " << 2 + 3 * 4 - 5 << endl;
+	cout << "14 + 8 * 2 = " << 14 + 8 * 2 << endl;
+	cout << "17 * (( 3 - 1 ) * 7 ) = " << 17 * ((3 - 1) * 7) << endl;
+	cout << "2.0 + 3.0 * (2.0 - 6.0 * 4.0) = " << 2.0 + 3.0 * (2.0 - 6.0 * 4.0) << endl;
+}
+
